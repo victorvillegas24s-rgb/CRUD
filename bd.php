5a10736fe54e47a08af62a15167c741c8602544d
YecsanJK <victor.villegas.24s@utzmg.edu.mx>
2025-10-27 17:38:53 -0600

    Agregando el mt Insercion para agrega los dt

--- bd.php
+++ bd.php
@@ -2,6 +2,14 @@
 $connection_string = "host=localhost port=5433 dbname=Simp user=postgres password=3315278732Mn";
 $con = pg_connect($connection_string);
 
+$mensaje_crud = "";
+
+if (isset($_POST['accion']) && $_POST['accion'] == 'crear') {
+    $nombre = pg_escape_string($con, $_POST['nombre']); 
+
+}
+
 $query = "SELECT id, nombre, tipo, edad FROM public.mascotas";
 $resultado = pg_query($con, $query);
+
 ?>
